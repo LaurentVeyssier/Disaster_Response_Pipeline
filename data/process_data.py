5673185e3ea1dcfa996ce151dcb74a61f68a85cb
Laurent VEYSSIER <lveyssier@gmail.com>
2023-01-10 20:36:54 +0100

    completed scripts

--- data/process_data.py
+++ data/process_data.py
@@ -1,29 +1,87 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Load and return 2 datasets
+    inputs:
+        messages_filepath: path to messages dataset
+        categories_filepath:  path to categories dataset
+    outputs:
+        messages: messages dataset
+        categories: categories dataset
+     """
+    
+    # load datasets
+    messages = pd.read_csv(messages_filepath, index_col=0)
+    categories = pd.read_csv(categories_filepath, index_col=0)
+
+    return messages, categories
 
 
-def clean_data(df):
-    pass
+
+
+def clean_data(messages, categories):
+    """combine 2 datasets, return the cleaned consolidated set
+    after adjustment of categories dataset 
+    inputs:
+        messages: raw messages dataset
+        categories: raw categories dataset
+    outputs:
+        df: consolidated dataset
+     """
+    # create a dataframe of the 36 individual category columns
+    categories = categories.categories.str.split(pat=';', expand=True)
+    # select the first row of the categories dataframe
+    first_row = categories.iloc[0,:].tolist()
+    # extract a list of new column names for categories.
+    category_colnames = [col_name[:-2] for col_name in first_row]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    # Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # Merge the messages and  new `categories` datasets using the common id
+    df = messages.join(categories, on='id')
+
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    # return clean consolidated dataset
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """Save cleaned dataframe to sql database using provided path
+    inputs:
+        df: cleaned dataframe
+        database_filename: path to database (str), ie '../data/DisasterResponse.db'
+    outputs:
+        None
+    """
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('messages', engine, index=False,if_exists='replace')  
 
 
 def main():
+    """pipeline to load raw data, preprocess and save cleaned dataset"""
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
 
+        messages, categories = load_data(messages_filepath, categories_filepath)
+        
         print('Cleaning data...')
-        df = clean_data(df)
+        df = clean_data(messages, categories)
         
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,32 +1,169 @@
-import sys
+import sys, re, pickle
+import pandas as pd
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+
+# for modeling tasks
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report, f1_score
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.compose import ColumnTransformer
+from xgboost import XGBClassifier
+
+# for NLP tasks
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+stop_words = stopwords.words("english")
+lemmatizer = WordNetLemmatizer()
+
+# NLP data augmentation
+import nlpaug.augmenter.word as naw
+aug = naw.SynonymAug(aug_src='wordnet')
 
 
-def load_data(database_filepath):
-    pass
+
+def load_data(database_filepath, augmentation=True):
+    """read sql table from database path and return segregated content
+    inputs:
+        - database_filepath: path to database (str)
+    outputs:
+        - X: dataframe with messages as str
+        - Y: labels (dataframe)
+        - category_names: list of classification labels (list of str)
+    """
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table(table_name='messages', con=engine)
+
+    if augmentation:
+        df = data_augmentation(df)
+
+    X = df.message #df.select_dtypes(include=['object']).drop('original',axis=1)
+    # There are 36 classes
+    Y = df.select_dtypes(include=['int64'])
+    category_names=Y.columns.tolist()
+    return X, Y, category_names
+
+
+def data_augmentation(dataframe):
+    # select classification labels with less than 1000 samples
+    labels = dataframe.select_dtypes(include=['int64']).columns.tolist()
+    sorted_labels = dataframe[labels].sum(axis=0).sort_values().index
+    under_represented_labels = sorted_labels[dataframe[labels].sum(axis=0).sort_values()<1000]
+
+    # perform data augmentation on under-represented classes
+    new_rows = []
+    df_to_augment = dataframe.loc[dataframe[under_represented_labels].sum(axis=1)==1,:]
+    for idx, row in df_to_augment.iterrows():
+        sample = row['message']
+        augmented_text = aug.augment(sample)
+        new_row = row.copy()
+        new_row['message'] = augmented_text[0]
+        new_rows.append(new_row)
+    
+    # combine new augmented rows with existing dataset
+    augmented_df = pd.DataFrame(new_rows)
+    augmented_df = pd.concat([dataframe, augmented_df], axis=0)
+    augmented_df.reset_index(drop=True, inplace=True)
+    return augmented_df
 
 
 def tokenize(text):
-    pass
+    """performs preprocessing on a string and return takenized text
+        - normalize text to lowercase
+        - strip all characters not a digit or a letter
+        - tokenize to words
+        - remove stopwords
+        - lemmatize nouns and verbs to root
+    inputs:
+        text: str
+    outputs:
+        tokens: list of tokens
+    """
+    # normalize case and remove punctuation
+    text = re.sub(r'[^a-zA-Z0-9]', ' ', text.lower())
+
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # lemmatize and remove stop words
+    tokens = [lemmatizer.lemmatize(token) for token in tokens if token not in stop_words]
+    tokens = [lemmatizer.lemmatize(token,pos='v') for token in tokens]
+    
+    return tokens
 
 
 def build_model():
-    pass
+    """Build ML pipeline for model training
+    inputs:
+        - None
+    outputs:
+        - ML model
+    """
+    pipeline = Pipeline([   ('tokenization', CountVectorizer(tokenizer=tokenize,
+                                                             min_df=3,
+                                                             max_features=10000)
+                                                             ),
+                        ('vectorization',TfidfTransformer()),
+                        ('modelisation', MultiOutputClassifier(estimator=XGBClassifier(), 
+                                                                n_jobs=-1)
+                                                                )
+                    ])
+    return pipeline
 
-
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, x, y, category_names,display_perf=False):
+    """Evaluate classification model performance
+    inputs
+        - model: trained ML model
+        - x: X_test features (dataframe)
+        - y: target label corresponding to X_text features (dataframe)
+        - category_names: list of str with classification labels
+        
+    """
+    yhat = model.predict(x)
+    labels = y.columns.tolist()
+    micro_score=[]
+    for i, name in enumerate(category_names):
+        print(f'******************** class: {labels[i]} *********************')
+        cr = classification_report(y.iloc[:,i], yhat[:,i], zero_division=0)
+        micro_score.append(round(f1_score(y.iloc[:,i], 
+                                          yhat[:,i],
+                                          average='macro'),2))
+        print(cr)
+        print()
+    perf = pd.DataFrame(micro_score, index=labels, columns=[name])
+    perf.plot(kind='bar', figsize=(15,5), label='XGBoostClassifier')
+    plt.title("Macro F1 score per label (unweighted)")
+    plt.tight_layout()
+    plt.savefig('model_performance.png')
+    if display_perf:
+        plt.show()
 
 
 def save_model(model, model_filepath):
-    pass
+    """save trained model to pickle file at provided filepath"""
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
+    """ pipeline to load dataset, segregate train / test data, 
+        assemble, train and savel ML model
+        data augmentation performed by default using nlpaug package
+    """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, 
+                                                            random_state=10, 
+                                                            test_size=0.2)
         
         print('Building model...')
         model = build_model()
@@ -46,7 +183,7 @@ def main():
         print('Please provide the filepath of the disaster messages database '\
               'as the first argument and the filepath of the pickle file to '\
               'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'train_classifier.py ../data/DisasterResponse.db classifier.pk')
 
 
 if __name__ == '__main__':
